a290ac9e587cf0191e5861ab60cecea55825006a
Gareth Ellis <gareth.ellis@iconplc.com>
2015-12-18 15:56:46 +0000

    Day 10 Puzzle 2 done - might be nice to find a way of doing it without having to set 2GB memory limit

--- src/Day10/Puzzle2.php
+++ src/Day10/Puzzle2.php
@@ -3,17 +3,20 @@
 namespace Day10;
 
 
-class Puzzle1
+class Puzzle2
 {
 	public function __invoke()
 	{
+		ini_set("memory_limit", "2G");
+
 		$look = new Look();
 
 		$string = "1321131112";
-		for ($i = 1; $i <= 40; $i++) {
+		for ($i = 1; $i <= 50; $i++) {
 			$string = $look->say($string);
 		}
 
+		echo "String: " . $string . PHP_EOL;
 		echo "Length: " . strlen($string) . PHP_EOL . PHP_EOL;
 	}
 }
